f1a5b49c2f653ea5fdd78909124480dbb2db2138
Telma <telcrika31@gmail.com>
2019-09-27 15:40:52 -0300

    fiz o exercicio 5, falta ativar botão

--- Exercício-05/js/script.js
+++ Exercício-05/js/script.js
@@ -10,7 +10,7 @@ const livros = {
         "link": "https://github.com/cezaraugusto/You-Dont-Know-JS"
     },
     3: {
-        "titulo": "Calibã e a Bruxa: mulheres, corpo e acumulação primitiva",
+        "titulo": "Caliba e a Bruxa: mulheres, corpo e acumulacao primitiva",
         "quemEscreveu": "Silvia Federici",
         "link": "http://coletivosycorax.org/wp-content/uploads/2019/09/CALIBA_E_A_BRUXA_WEB-1.pdf"
     }
@@ -21,19 +21,41 @@ const ulMae = document.getElementById("lista-do-catalogo");
 
 
 
-for (let titulos in livros) {
-    let prateleira = document.createElement("h3");
+for (let propriedade in livros) {
+
+    let prateleira = document.createElement("li");
     ulMae.appendChild(prateleira);
-    prateleira.textContent = livros[titulos]["titulo"];
+    prateleira.classList.add("livros");
+    
+    const titulo = document.createElement("h3");
+    ulMae.appendChild(titulo);
+    titulo.textContent = livros[propriedade]["titulo"];
+    titulo.classList.add("livro__titulo");
+
 
     const escritor = document.createElement("p");
     ulMae.appendChild(escritor);
-    escritor.textContent = livros[titulos]["quemEscreveu"];
+    escritor.textContent = livros[propriedade]["quemEscreveu"];
+    escritor.classList.add("livro__autoria");
 
-    const site = document.createElement("a");
+    const site = document.createElement("href");
     ulMae.appendChild(site);
-    site.textContent = livros[titulos]["link"];
+    site.textContent = "clique aqui";
+    site.setAttribute = 'a',  livros[propriedade]["link"];
+    site.classList.add("livro__link");
+
+    const botao = document.createElement("button");
+    ulMae.appendChild(botao);
+    botao.textContent = ["Ja li"];
+    botao.classList.add("botao-lido");
+
+    const botaoDesfazer = document.createElement("button");
+    ulMae.appendChild(botaoDesfazer);
+    botaoDesfazer.textContent = ["Desfazer"];
+    botaoDesfazer.classList.add("botao-desfazer");
 
 }
 
-   
+ // Toggle =  quando o botão executa duas funções diferentes quando aperta e desaperta  
+
+//FUNÇÃO CALLBACK = Função que é passada para um aoutra como parametro uma função
